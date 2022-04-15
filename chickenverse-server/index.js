86b4f42468c8aa9b961eef7aecd97a7313df2973
Nathan Pham <nathanpham.me@gmail.com>
2022-04-14 20:36:17 -0700

    mint egg animation

--- chickenverse-server/index.js
+++ chickenverse-server/index.js
@@ -1,5 +1,53 @@
 import { Server } from "socket.io";
+import PolySchema, { PolyTypes } from "the-poly-schema";
 
-const io = new Server(3001);
+// create new io server
+const io = new Server(3001, {
+    cors: {
+        origin: "*",
+    },
+});
 
-io.on("connection", (socket) => {});
+// store chickens in memory
+const chickens = {};
+
+// chicken schema
+const chickenSchema = new PolySchema({
+    id: PolyTypes.number,
+    traits: PolyTypes.arrayOf(PolyTypes.string),
+    pos: {
+        x: PolyTypes.number,
+        y: PolyTypes.number,
+    },
+    isFlipped: PolyTypes.boolean,
+});
+
+io.on("connection", (socket) => {
+    socket.on("chicken", (data) => {
+        if (chickenSchema.validate(data, true)) {
+            chickens[data.id] = data;
+        }
+    });
+
+    socket.on("request_chicken", (id) => {
+        socket.emit(
+            "respond_chicken",
+            chickens[id] || {
+                id: 0,
+                traits: [],
+                pos: {
+                    x: 100,
+                    y: 100,
+                },
+            }
+        );
+    });
+
+    const interval = setInterval(() => {
+        socket.emit("chickens", chickens);
+    }, 1000 / 60);
+
+    socket.on("disconnect", () => {
+        clearInterval(interval);
+    });
+});
